1f1d3cc0f16d26bbb8872a89ea0ae37a191d3c8e
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-05 00:35:24 +0100

    here i learned Manipulate a html content using DOM, and i worked on score logic

--- script.js
+++ script.js
@@ -7,10 +7,41 @@
 // document.querySelector('.guess').value = 30;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.floor(Math.random()*20)+1;
+let score = 20; // called state varible cuz we have it in html 
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
     console.log(guess, typeof guess)
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number!'
+    }
+    else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!';
+        score++;
+        document.querySelector('.score').textContent = score;
+    }
+
+    else if (guess < secretNumber) {
+        if(score > 1){
+            document.querySelector('.message').textContent = 'too low!'
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else{document.querySelector('.message').textContent = 'you lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+
+      else if (guess > secretNumber) {
+        if (score > 1) {
+        document.querySelector('.message').textContent = 'too high!'
+        score--;
+        document.querySelector('.score').textContent = score;            
+        }else{document.querySelector('.message').textContent = 'you lost the game';
+            document.querySelector('.score').textContent = 0
+        }
+
     }
 });
